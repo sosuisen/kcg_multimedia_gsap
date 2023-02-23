e845ed92d0577c786cdeba4a050098100624742c
sosuisen <hidekazu.kubota@gmail.com>
2023-02-23 22:42:07 +0900

    fix: kihon

--- kihon.js
+++ kihon.js
@@ -1,1 +1,3 @@
-gsap.to("#b1", { y: -200 });
+gsap.fromTo("#b1", { alpha: 0 },{ duration: 1, rotation: -90, alpha: 0.3 });
+gsap.fromTo("#b2", { alpha: 0 },{ duration: 1, rotation: -45, alpha: 0.3 });
+gsap.fromTo("#b3", { alpha: 0 },{ duration: 1, rotation: -67, alpha: 0.3 });
