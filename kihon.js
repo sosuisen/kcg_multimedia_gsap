14ed1ff431bc88784f9715053e7bf6f8bef52027
sosuisen <hidekazu.kubota@gmail.com>
2023-02-23 22:40:57 +0900

    feat: add answer

--- kihon.js
+++ kihon.js
@@ -1,4 +1,7 @@
-gsap.to("#b1", { y: -200 });
-gsap.to("#b2", { y: -150 });
-gsap.to("#b3", { y: -100 });
+gsap.fromTo("#b1", { alpha: 0 },{ duration: 1, rotation: -90, alpha: 0.3 });
+gsap.fromTo("#b2", { alpha: 0 },{ duration: 1, rotation: -45, alpha: 0.3 });
+gsap.fromTo("#b3", { alpha: 0 },{ duration: 1, rotation: -67, alpha: 0.3 });
 
+gsap.to("#b1", { y: -200, delay: 1, duration: 0.8, rotation: 0, alpha: 1.0 });
+gsap.to("#b2", { y: -150, delay: 1, duration: 1.0, rotation: 0, alpha: 1.0 });
+gsap.to("#b3", { y: -100, delay: 1, duration: 1.2, rotation: 0, alpha: 1.0 });
